1c017e124eb76fc11c5acfe84463b0c8516f4b7f
Martins <martins.tarando@gmail.com>
2025-04-01 15:10:58 +0300

    CrossOrigin updated

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"https://kaleidoscopic-pavlova-fd05ac.netlify.app"})
 @Tag(name = "Produkti", description = "Produktu pārvaldība")
 @Validated
 @RestController
